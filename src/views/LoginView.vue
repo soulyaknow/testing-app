4a4ff0bcf4d686d61691d2c5066b195b4e366702
soulyaknow <ranel.soliano@gmail.com>
2023-09-03 20:50:02 -0700

    first commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login-container">
+    <div class="left-side">
+      <div class="logo">
+        <a href="">
+          <img src="../assets/cpc_logo.png" width="90" height="90" alt="CPC Logo">
+        </a>
+      </div>
+    </div><!-- End of left-side -->
+    <div class="right-side">
+      <h1>Login</h1>
+      <form action="#" method="POST">
+        <input type="text" name="student-id" placeholder="Student ID" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit" id="submit" value="submit">Login</button>
+      </form>
+    </div><!-- End of right-side -->
+  </div><!-- End of login-container -->
+</template>
+<script>
+
+</script>
